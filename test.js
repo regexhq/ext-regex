f13a1f187d075a620745a8c286f13a519a878154
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-12 23:25:08 -0500

    add tests

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * ext-regex <https://github.com/regexps/ext-regex>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015 Jon Schlinkert.
  * Licensed under the MIT License
  */
 
@@ -10,28 +10,55 @@
 var assert = require('assert');
 var re = require('./');
 
+function match(str) {
+  return str.match(re());
+}
+
 it('should match a file extension:', function () {
-  assert.equal(re().exec('.md')[0], '.md');
-  assert.equal(re().exec('.md')[1], 'md');
-  assert.equal(re().exec('abc.md')[0], '.md');
-  assert.equal(re().exec('abc.md')[1], 'md');
+  assert.equal(re().test('.md'), true);
+  assert.equal(match('.md')[0], '.md');
+  assert.equal(match('.md')[1], '.md');
+  assert.equal(match('.md')[2], 'md');
+  assert.equal(match('abc.md')[0], '.md');
+  assert.equal(match('abc.md')[1], '.md');;
+  assert.equal(match('abc.md')[2], 'md');
 });
 
-it('should not match a double extension:', function () {
-  assert.equal(re().exec('abc.min.js')[0], '.js');
-  assert.equal(re().exec('abc.min.js')[1], 'js');
+it('should not match if no extension is found:', function () {
+  assert.equal(re().test('abc'), false);
+  assert.equal(match(''), null);
+  assert.equal(match('abc'), null);
+  assert.equal(match('a/b/c'), null);
+});
+
+it('should match multiple extensions:', function () {
+  assert.equal(match('a/b/c/foo.bar.abc.min.js')[0], '.bar.abc.min.js');
+  assert.equal(match('a/b/c/foo.bar.abc.min.js')[1], '.js');
+  assert.equal(match('a/b/c/foo.bar.abc.min.js')[2], 'js');
+  assert.equal(match('a/b/c/bar.abc.min.js')[0], '.abc.min.js');
+  assert.equal(match('a/b/c/bar.abc.min.js')[1], '.js');
+  assert.equal(match('a/b/c/bar.abc.min.js')[2], 'js');
+  assert.equal(match('a/b/c/abc.min.js')[0], '.min.js');
+  assert.equal(match('a/b/c/abc.min.js')[1], '.js');
+  assert.equal(match('a/b/c/abc.min.js')[2], 'js');
+  assert.equal(match('abc.min.js')[0], '.min.js');
+  assert.equal(match('abc.min.js')[1], '.js');
+  assert.equal(match('abc.min.js')[2], 'js');
+});
+
+it('should work with dirnames that have dots:', function () {
+  assert.equal(re().test('a/b/c.d.e/'), false);
+  assert.equal(re().test('a/b/c.d.e/f.js'), true);
+  assert.equal(match('a/b/c.d.e/f.js')[0], '.js');
+  assert.equal(match('a/b/c.d.e/'), null);
 });
 
 it('should match a dotfile:', function () {
-  assert.equal(re().exec('abc/.gitignore')[0], '.gitignore');
-  assert.equal(re().exec('abc/.gitignore')[1], 'gitignore');
-});
-
-it('should match a double extension when `last: true`:', function () {
-  assert.equal(re(true).exec('abc.min.js')[0], '.min.js');
-  assert.equal(re(true).exec('abc.min.js')[1], 'min.js');
-  assert.equal(re(true).exec('abc.foo.min.js')[0], '.foo.min.js');
-  assert.equal(re(true).exec('abc.foo.min.js')[1], 'foo.min.js');
-  assert.equal(re(true).exec('a/.b/abc.foo.min.js')[0], '.foo.min.js');
-  assert.equal(re(true).exec('a/.b/abc.foo.min.js')[1], 'foo.min.js');
+  assert.equal(re().test('.gitignore'), true);
+  assert.equal(match('.gitignore')[0], '.gitignore');
+  assert.equal(match('.gitignore')[1], '.gitignore');
+  assert.equal(match('.gitignore')[2], 'gitignore');
+  assert.equal(match('abc/.gitignore')[0], '.gitignore');
+  assert.equal(match('abc/.gitignore')[1], '.gitignore');
+  assert.equal(match('abc/.gitignore')[2], 'gitignore');
 });
